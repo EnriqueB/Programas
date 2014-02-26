3ea21aa2c9d9a6732acf56f717dee03b02250ed4
Enrique Bonilla <enriqueBonilla93@outlook.com>
2014-02-26 00:48:57 -0600

    Programa 2312, sin completar

--- TopTecProgrammer2014/Encoding-2312/2312.cpp
+++ TopTecProgrammer2014/Encoding-2312/2312.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <string.h>
 
 using namespace std;
 
 string convertir(char a){
 	switch (a){
+		case ' ':
+			return "00000";
+			break;
 		case 'A':
 			return "00001";
 			break;
@@ -86,31 +91,72 @@ string convertir(char a){
 	return "";
 }
 int main(){
-	int n, apS=0, reng, col, apR=0, apC=0; dir=0, matriz[20][20];
+	int n, caso=1;
 	string inp="", cod="";
 	cin>>n;
 	while(n--){
-		apS=apR=apC=dir=0;
-		cin>>reng>>col>>inp;
+		cod="";
+		char matriz[20][20];
+		int apS=0, reng, col, apR=0, apC=0, dir=0, r, c; 
+		cin>>reng>>col;
+		getline(cin, inp);
+		inp.erase(0,1);
 		for(int i=0; i<inp.length(); i++){
 			cod+=convertir(inp[i]);
 		}	
 		while(cod.length()<reng*col){
 			cod.insert(cod.end(), '0');
-		} 
+		}
+		r=reng-1;
+		c=col;
 		while(apS<reng*col){
 			switch (dir){
 				case 0:
-					for(int i=0; i	
+					for(int i=0; i<c; i++){
+						matriz[apR][apC++]=cod[apS++];
+					}
+					apC--;
+					dir++;
+					apR++;
+					c--;
 					break;
 				case 1:
+					for(int i=0; i<r; i++){
+						matriz[apR++][apC]=cod[apS++];
+					}
+					apR--;
+					dir++;
+					apC--;
+					r--;
 					break;
 				case 2:
+					for(int i=0; i<c; i++){
+						matriz[apR][apC--]=cod[apS++];
+					}
+					apC++;	
+					dir++;
+					apR--;
+					c--;
 					break;
 				case 3:
+					for(int i=0; i<r; i++){
+						matriz[apR--][apC]=cod[apS++];
+					}
+					apR++;
+					dir=0;
+					apC++;
+					r--;
 					break;
+			}	
+		}
+		cout<<caso<<" ";
+		for(int i=0; i<reng; i++){
+			for(int j=0; j<col; j++){
+				cout<<matriz[i][j];
 			}
 		}	
+		cout<<endl;
+		caso++;
 	}
 	return 0;
 }
